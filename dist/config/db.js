b7f9be6db3a2d4570045446f3788d1464d9c4171
shadrxcc <akaadesm@gmail.com>
2025-07-16 23:45:28 +0100

    add dotenv

--- dist/config/db.js
+++ dist/config/db.js
@@ -4,6 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const connectDB = async () => {
     try {
         const uri = process.env.MONGODB_URI;
